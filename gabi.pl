192a206f246514df7ee319d595087c9e98d6fb82
lawless-m <matt-github@steponnopets.net>
2021-07-26 08:41:07 +0100

    added print

--- gabi.pl
+++ gabi.pl
@@ -2,8 +2,22 @@
 % https://logic-masters.de/Raetselportal/Raetsel/zeigen.php?id=0003XY
 % gabi([G,E,B,U,R,T,S,T,A,G]) , label([G,E,B,U,R,T,S,T,A,G]).
 
-gabi([G,E,B,U,R,T,S,T,A,G]) :-
+zipp([], [], []).
+zipp([A|As], [B|Bs], [A-B|Zs]) :- zipp(As, Bs, Zs).
+
+printV(_, []).
+printV(L, [K-V|KVs]) :-
+	(L=K, write(V)) ; printV(L, KVs).
+
+printVs([], _).
+printVs([L|Letters], Db) :-
+	printV(L, Db),
+	printVs(Letters, Db).
+
+gabi(Vars, Names, VNs) :-
 	Vars = [A,B,C,D,E,F,G,H,I,L,N,R,S,T,U,W,Z],
+	Names = [a,b,c,d,e,f,g,h,i,l,n,r,s,t,u,w,z],
+	zipp(Names, Vars, VNs),
 	length(Vars, Nvars),
 	Vars ins 1..Nvars,
 	all_different(Vars),
@@ -21,4 +35,5 @@ gabi([G,E,B,U,R,T,S,T,A,G]) :-
 	G+U+T+E #= 43,
 	F+U+E+R #= 48,
 	D+I+C+H #= 32.
+	
 
